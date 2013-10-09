b89ff428eb772b861d98b5c98fe332a3cde16234
Adam Boxall <adam.boxall@bskyb.com>
2013-10-09 15:29:27 +0100

    Added language file

---
+++ language/english/blurbs_lang.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+// labels
+$lang['streams:blurbs.name'] = 'Blurbs';
+$lang['streams:blurbs.return_type'] = '';
+$lang['streams:blurbs.return_type.instructions'] = '';
