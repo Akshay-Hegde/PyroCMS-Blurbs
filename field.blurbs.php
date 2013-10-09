fd7987ea76c8b97eb5ef6a73e5aff5456da1dc4f
Adam Boxall <adam.boxall@bskyb.com>
2013-10-09 15:17:34 +0100

    Initial commit

---
+++ field.blurbs.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Blurbs field type
+ *
+ * A custom field type for PyroCMS that provides a list of title and body blurbs
+ *
+ * @package Addons\Field Types
+ * @author  Adam Boxall
+ * @license MIT License
+ * @link http://github.com/AdamBoxall/BlurbFieldType
+ */
+class Field_blurbs
+{
+    public $field_type_slug = 'blurbs';
+    public $db_col_type = 'text';
+    public $version = '1.0.0';
+    public $author = array(
+        'name' => 'Adam Boxall',
+        'url' => 'http://adamboxall.com'
+    );
+
+    public function form_output($data)
+    {
+        return (!$data['value'])
+            ? $this->getCleanForm($data['form_slug'])
+            : $this->getUsedForm($data);
+    }
+
+    protected function getCleanForm($formSlug)
+    {
+        ob_start();
+        require __DIR__ . '/views/form.php';
+        return ob_get_clean();
+    }
+
+    protected function getUsedForm(array &$data)
+    {
+        $formSlug = $data['form_slug'];
+        $items = unserialize($data['value']);
+
+        ob_start();
+        require __DIR__ . '/views/form.php';
+        return ob_get_clean();
+    }
+
+    public function pre_save($input)
+    {
+        return serialize($input);
+    }
+
+    public function pre_output($data)
+    {
+        return unserialize($data);
+    }
+}
---
+++ views/form.php
@@ -0,0 +1,43 @@
+<ul class="blurbs" id="<?php echo $formSlug ?>">
+    <?php if (isset($items)): ?>
+        <?php foreach ($items as $i => &$item): ?>
+            <li>
+                <ul class="blurbs-input">
+                    <li class="input">
+                        <input type="text" name="<?php echo $formSlug ?>[<?php echo $i ?>][title]" placeholder="Title" value="<?php if (isset($item['title'])) echo $item['title'] ?>">
+                    </li>
+                    <li class="input">
+                        <input type="text" name="<?php echo $formSlug ?>[<?php echo $i ?>][link]" placeholder="Link" value="<?php if (isset($item['link'])) echo $item['link'] ?>">
+                    </li>
+                    <li class="input">
+                        <textarea name="<?php echo $formSlug ?>[<?php echo $i ?>][body]" placeholder="Body"><?php if (isset($item['body'])) echo $item['body'] ?></textarea>
+                    </li>
+                </ul>
+                <div class="blurbs-remove">
+                    <a href="#">Remove</a>
+                </div>
+            </li>
+        <?php endforeach ?>
+    <?php else: ?>
+        <li>
+            <ul class="blurbs-input">
+                <li class="input">
+                    <input type="text" name="<?php echo $formSlug ?>[<?php echo $i ?>][title]" placeholder="Title">
+                </li>
+                <li class="input">
+                    <input type="text" name="<?php echo $formSlug ?>[<?php echo $i ?>][link]" placeholder="Link">
+                </li>
+                <li class="input">
+                    <textarea name="<?php echo $formSlug ?>[<?php echo $i ?>][body]" placeholder="Body"></textarea>
+                </li>
+            </ul>
+            <div class="blurbs-remove">
+                <a href="#">Remove</a>
+            </div>
+        </li>
+    <?php endif ?>
+</ul>
+
+<div class="blurb-add">
+    <a href="#">Add</a>
+</div>
